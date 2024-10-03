11acdd2a67d504aed9e986b4970e25f30a816ba5
LyenZo <al222310597@gmail.com>
2024-10-02 23:34:22 -0600

    Se hace el merge entre main y rama3

--- resources/views/git.blade.php
+++ resources/views/git.blade.php
@@ -10,5 +10,6 @@
     <<!Primer comentario dentro de rama1>>
     <<!Segundo comentario dentro de rama2>>
     <<!Tercer comentario dentro de rama3>>
+    <<!Se hace el merge entre main y rama3>>
 </body>
 </html>
